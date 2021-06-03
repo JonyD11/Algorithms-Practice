eb48afd3c8e4032a3da0a3589499dfdd3d45a17e
Tanveer <johnnydalal11@gmail.com>
2021-06-03 17:25:51 +0100

    Bubble Sort Implementation

---
+++ Sorting Algorithms/src/Bubble_Sort.java
@@ -0,0 +1,40 @@
+
+public class Bubble_Sort extends Sort {
+	
+	/*
+	 * Bubble Sort is the simplest sorting algorithm that works by 
+	 * repeatedly swapping the adjacent elements if they are in wrong
+	 * order.
+	 * */
+	
+	void Sort (int[] arr)
+	{
+		int size = arr.length;
+		for (int i = 0; i < size; i++)
+			for (int j = 1; j < size; j++)
+				if (arr[j] < arr[j - 1])
+				{
+					swap(arr, j, j - 1);
+				}
+	}
+	
+	// Recursive Bubble Sort
+	void Sort1 (int[] arr, int size)
+	{
+		// Base case
+		if (size == 1)
+			return;
+		
+		// One pass of bubble sort. After this pass, the largest element 
+		// is moved (or bubbled) to end
+		for (int i = 0; i < size - 1; i++)
+			if (arr[i] > arr[i + 1])
+			{
+				swap(arr, i, i + 1);
+			}
+		
+		// Largest element is fixed, recur for remaining array
+		Sort1(arr, size - 1);
+	}
+
+}
--- Sorting Algorithms/src/main.java
+++ Sorting Algorithms/src/main.java
@@ -4,6 +4,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// Selection Sort Implementation
 		Selection_Sort ob = new Selection_Sort();
 		
 		int arr[] = {53, 22, 48, 15, 10, 5, 75};
@@ -12,7 +13,25 @@ public class main {
 		ob.printArray(arr);
 		System.out.println();
 		
+		// Bubble Sort Implementation
+		Bubble_Sort ob1 = new Bubble_Sort();
 		
+		int arr1[] = {53, 22, 48, 15, 10, 5, 75};
+		ob1.Sort(arr1);
+		System.out.println("Bubble Sort");
+		ob1.printArray(arr1);
+		System.out.println();
+		
+		// Recursive Bubble Sort Implementation
+		Bubble_Sort ob1_1 = new Bubble_Sort();
+		
+		int arr1_1[] = {53, 22, 48, 15, 10, 5, 75};
+		ob1_1.Sort1(arr1_1, arr1_1.length);
+		System.out.println("Recursive Bubble Sort");
+		ob1_1.printArray(arr1_1);
+		System.out.println();
+		
+		//
 		
 	}
 
